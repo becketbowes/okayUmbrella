1dc08cd295a2583f0f542b17c27422ad36847b71
becket <becket.bowes@gmail.com>
2022-03-13 14:10:18 -0400

    initial models sketched out

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //references product model id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //references tag model's id
+    } 
   },
   {
     sequelize,
